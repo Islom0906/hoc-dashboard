ba8d3779f537d71134dd583eceacbffaa29d3ee3
islom <iabdugofurov99@gmail.com>
2024-06-14 11:33:27 +0500

    Bismillah

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import AppLayout from "./AppLayout";
+import {ConfigProvider} from "antd";
+import {theme} from "./constants/theme";
+function App() {
+  return (
+    <div>
+        <ConfigProvider theme={theme}>
+        <AppLayout/>
+        </ConfigProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/theme.js
@@ -0,0 +1,8 @@
+
+export const theme = {
+    token: {
+        colorPrimary: '#a80000',
+        colorBgContainer:"#000000"
+    },
+};
+
