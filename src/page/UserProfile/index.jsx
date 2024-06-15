2742d371ca6af1fd37b82ac8421b9809f5744463
BoburBerdiev <sirofim012@gmail.com>
2024-06-15 18:27:57 +0500

    task 11%

--- src/page/UserProfile/index.jsx
+++ src/page/UserProfile/index.jsx
@@ -1,17 +1,27 @@
 import './index.scss'
-import {Col, Input, Row, Form, Button, Upload} from "antd";
+import {Col, Input, Row, Form, Button, Upload, Space, DatePicker} from "antd";
 import {PlusOutlined} from "@ant-design/icons";
 
 
 const UserProfile = () => {
   return (
-      <div className={'UserProfile'}>
+      <Space direction={"vertical"}  className={'UserProfile'}>
         <h1>
           User setting
         </h1>
         <Form layout="vertical"
         >
           <Row gutter={12}>
+            <Col span={24}>
+              <Form.Item label="Your image" >
+                <Upload action="/upload.do" listType="picture-card">
+                  <button style={{ border: 0, background: 'none' }} type="button">
+                    <PlusOutlined />
+                    <div style={{ marginTop: 8 }}>Upload</div>
+                  </button>
+                </Upload>
+              </Form.Item>
+            </Col>
             <Col className="gutter-row" span={12}>
               <Form.Item label="First name">
                 <Input placeholder="admin"/>
@@ -37,16 +47,13 @@ const UserProfile = () => {
                 <Input placeholder="Manager"/>
               </Form.Item>
             </Col>
-            <Col>
-              <Form.Item label="Your image" >
-                <Upload action="/upload.do" listType="picture-card">
-                  <button style={{ border: 0, background: 'none' }} type="button">
-                    <PlusOutlined />
-                    <div style={{ marginTop: 8 }}>Upload</div>
-                  </button>
-                </Upload>
+            <Col className="gutter-row" span={12}>
+              <Form.Item label="DatePicker">
+                <DatePicker style={{width:'100%'}} />
               </Form.Item>
             </Col>
+
+
             <Col  span={24}>
               <Button type={"primary"}  size={'large'}>
                 Save changes
@@ -55,7 +62,7 @@ const UserProfile = () => {
           </Row>
         </Form>
 
-      </div>
+      </Space>
   );
 };
 
