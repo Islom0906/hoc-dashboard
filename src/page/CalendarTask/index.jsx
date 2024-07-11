50d6fc106c3bc5cf29148483007d05a9352277db
islom <iabdugofurov99@gmail.com>
2024-07-11 11:01:43 +0500

    calendarga meeting qoshilvoti

--- src/page/CalendarTask/index.jsx
+++ src/page/CalendarTask/index.jsx
@@ -1,154 +1,44 @@
-import React, { useState} from 'react';
-import {Alert, Badge, Calendar, Modal, Space} from 'antd';
-import dayjs from 'dayjs';
+import React, {useEffect} from 'react';
+import {message, Space, Spin} from 'antd';
+import {useQuery} from "react-query";
+import apiService from "../../service/apis/api";
+import CustomCalendar from "./CustomCalendar";
 
-const birthdays=[
-    {
-        "id": 20,
-        "first_name": "Boss Staff User1",
-        "last_name": "Boss Staff User",
-        "birthday": "09.10.1998",
-        "roles": [
-            {
-                "id": 3,
-                "name": "staff"
-            }
-        ]
-    },
-    {
-        "id": 17,
-        "first_name": "Bobur",
-        "last_name": "Berdiev",
-        "birthday": "10.10.2002",
-        "roles": [
-            {
-                "id": 2,
-                "name": "boss"
-            }
-        ]
-    },
-    {
-        "id": 18,
-        "first_name": "Javlon",
-        "last_name": "Baxtiyorov",
-        "birthday": "09.10.2006",
-        "roles": [
-            {
-                "id": 3,
-                "name": "staff"
-            }
-        ]
-    },
-    {
-        "id": 19,
-        "first_name": "Qudrat",
-        "last_name": "Qudrat",
-        "birthday": "09.10.1999",
-        "roles": [
-            {
-                "id": 1,
-                "name": "admin"
-            }
-        ]
-    },
-    {
-        "id": 25,
-        "first_name": "Islom",
-        "last_name": "Abdugofurov",
-        "birthday": "07.01.2024",
-        "roles": [
-            {
-                "id": 3,
-                "name": "staff"
-            }
-        ]
-    },
-    {
-        "id": 29,
-        "first_name": "Aziz",
-        "last_name": "aziz",
-        "birthday": "04.07.2024",
-        "roles": [
-            {
-                "id": 3,
-                "name": "staff"
-            }
-        ]
-    }
-]
 
 const CalendarTask = () => {
-    const [value, setValue] = useState(() => dayjs());
-    const [selectedValue, setSelectedValue] = useState(() => dayjs());
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const {
+        data: dataBirthDay,
+        isLoading: getLoading,
+        refetch: refetchBirthDay,
+    } = useQuery('birthDay-get', () => apiService.getData(`/users/user-birthdays/`), {
+        enabled: false,
+        onError: (error) => {
 
-    const onSelect = (newValue) => {
-        setValue(newValue);
-        setSelectedValue(newValue);
-        setIsModalOpen(true)
-    };
-    const onPanelChange = (newValue) => {
-        setValue(newValue);
-    };
+            message.error(error.message);
+            // Handle the error
+        },
+    });
 
-    const birthdayMap = birthdays.reduce((acc, birthday) => {
-        console.log(acc)
-        const date = dayjs(birthday.birthday, 'DD.MM.YYYY'); // convert to moment object
-        const key = date.format('YYYY-MM-DD'); // get the YYYY-MM-DD format
-        if (!acc[key]) {
-            acc[key] = [];
-        }
-        acc[key].push(birthday);
-        return acc;
-    }, {});
-
-    const dateCellRender = (value) => {
-        const dateStr = value.format('YYYY-MM-DD');
-        const birthdaysOnDate = birthdayMap[dateStr] || [];
-        return (
-            <ul className="events">
-                {birthdaysOnDate.map(birthday => (
-                    <li key={birthday.id}>
-                        <Badge status="success" text={`${birthday.first_name} ${birthday.last_name}`} />
-                    </li>
-                ))}
-            </ul>
-        );
-    };
-
-
-
-
-
-
+    useEffect(() => {
+        refetchBirthDay()
+    }, []);
     return (
+        <Spin spinning={getLoading}>
         <Space direction={"vertical"} size={20}>
             <h1>
                 Calendar
             </h1>
-            <Alert message={`You selected date: ${selectedValue?.format('YYYY-MM-DD')}`}/>
-            <Calendar value={value} onSelect={onSelect} onPanelChange={onPanelChange} cellRender={dateCellRender}/>
-            <DayModalTaskList title={`${selectedValue?.format('YYYY-MM-DD')} daily task list`} isModalOpen={isModalOpen}
-                              setIsModalOpen={setIsModalOpen}/>
+            <CustomCalendar
+
+                dataBirthDay={dataBirthDay}
+            />
         </Space>
+        </Spin>
     );
 };
 
-export const DayModalTaskList = ({isModalOpen, setIsModalOpen, title}) => {
-    const handleOk = () => {
-        setIsModalOpen(false);
-    };
-    const handleCancel = () => {
-        setIsModalOpen(false);
-    };
-    return (
-        <Modal title={title} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-        </Modal>
-    );
 
-}
 export default CalendarTask;
+
+
 
