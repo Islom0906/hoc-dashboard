aabf84be4163e7a7d05bebe8a292857c32f05165
bobur <sirofim012@gmail.com>
2024-10-16 14:57:45 +0500

    1.5 - task done

--- src/page/TaskList/TaskInner/TaskHeader.jsx
+++ src/page/TaskList/TaskInner/TaskHeader.jsx
@@ -1,134 +1,155 @@
-import {Button, Checkbox, Col, Flex, Popconfirm, Row, Spin, Tag, Typography} from "antd";
-import React, {useEffect,  useState} from "react";
-import {useEditQuery} from "../../../service/query/Queries";
-import {FaRegCommentDots} from "react-icons/fa";
-import {useSelector} from "react-redux";
-import {IoMdCheckboxOutline} from "react-icons/io";
-import {CgDanger} from "react-icons/cg";
-const {Text, Title} = Typography;
+import {Button, Col, Row, Spin, Tag, Typography, Flex, Popconfirm} from "antd";
+import React, { useEffect, useState } from "react";
+import { useEditQuery } from "../../../service/query/Queries";
+import { FaRegCommentDots } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { IoMdCheckboxOutline } from "react-icons/io";
+import { CgDanger } from "react-icons/cg";
 
+const { Text, Title } = Typography;
 
-const TaskHeader = ({title, text,  task_status , id ,showModal ,setWhichWriteIDTask ,creatBy}) => {
-  const {data: {user} = {}} = useSelector((state) => state.auth);
+const TaskHeader = ({ title, text, task_status, id, showModal, setWhichWriteIDTask, creatBy }) => {
+  const { data: { user } = {} } = useSelector((state) => state.auth);
 
   const [checkedState, setCheckedState] = useState({});
-  const {
-    mutate: putProjectDone,
-    isLoading: putProjectDoneLoading,
-    isSuccess: putProjectDoneSuccess
-  } = useEditQuery()
-  const roles = user?.roles[0]?.role?.name
+  const { mutate: updateTaskStatus, isLoading: isUpdating } = useEditQuery();
+  const roles = user?.roles[0]?.role?.name;
 
   useEffect(() => {
-    setCheckedState((prevState) => ({...prevState, [id]: task_status === 'checking' ? true : false}));
-  }, [id])
+    setCheckedState((prevState) => ({ ...prevState, [id]: task_status === 'checking' }));
+  }, [id, task_status]);
 
   const clickHandle = (id) => {
-    showModal()
-    setWhichWriteIDTask(id)
-  }
+    showModal();
+    setWhichWriteIDTask(id);
+  };
 
-  const handleStatusChecking = (id) => {
-    // if(task_status === 'progress') {
-      putProjectDone({url: `/users/tasks-update`, data: {status: 'checking'}, id})
-    // }
-  }
-  const handleStatusDone = (id) => {
-    // if(task_status === 'progress') {
-    putProjectDone({url: `/users/tasks-update`, data: {status: 'done'}, id})
-    // }
-  }
-  const handleStatusProgress = (id) => {
-    // if(task_status === 'progress') {
-    putProjectDone({url: `/users/tasks-update`, data: {status: 'progress'}, id})
-    // }
-  }
+  const updateStatus = (id, newStatus) => {
+    updateTaskStatus({ url: `/users/tasks-update`, data: { status: newStatus }, id });
+  };
 
+  const taskStatusChecking = () => {
+    if(roles === 'general_director'|| roles === 'director') {
+      if(task_status === 'done'){
+        return <Tag color="green">Done</Tag>
+      }else if(task_status === 'progress'){
+        return <Tag color="green">Progress</Tag>
+      }else if(task_status === 'checking'){
+        return <Tag color="green">Checking</Tag>
+      }
+    }else if(roles === 'admin' ){
+      if(task_status === 'done'){
+        return <Tag color="green">Done</Tag>
+      }else if(task_status === 'progress'){
+        return <Tag color="green">Progress</Tag>
+      }else if(task_status === 'checking'){
+        return(<>
+        <Popconfirm
+            cancelText={'Отменить'}
+            okText={'Завершить'}
+            title={'Завершить задачу'}
+            description={'Вы уверены, что хотите завершить задачу?'}
+            onConfirm={() => updateStatus(id, 'done')}
+        >
+          <Button type="primary">
+            <IoMdCheckboxOutline /> Done
+          </Button>
+        </Popconfirm>
+        <Popconfirm
+            cancelText={'Отменить'}
+            okText={'Завершить'}
+            title={'Завершить задачу'}
+            description={'Вы уверены, что хотите завершить задачу?'}
+            onConfirm={() => updateStatus(id, 'progress')}
+        >
+            <Button type="danger">
+              <CgDanger /> Set to Progress
+            </Button>
+        </Popconfirm>
+          </>)
+      }else if(roles === 'staff' ){
+        if(task_status === 'done'){
+          return <Tag color="green">Done</Tag>
+        }else if(task_status === 'progress'){
+          return<Popconfirm
+              cancelText={'Отменить'}
+              okText={'Завершить'}
+              title={'Завершить задачу'}
+              description={'Вы уверены, что хотите завершить задачу?'}
+              onConfirm={() => updateStatus(id, 'checking')}
+          >
+          <Button type="primary">
+            <IoMdCheckboxOutline /> Set to Checking
+          </Button>
+          </Popconfirm>
+        }else if(task_status === 'checking'){
+          return<Tag color="green">Checking</Tag>
+        }else {
+        console.log('aaa')
+        }
+      }
+    }
+  }
 
   return (
       <div>
-        <Spin spinning={putProjectDoneLoading}>
-          <Row style={{width: '100%'}}>
+        <Spin spinning={isUpdating}>
+          <Row style={{ width: "100%" }}>
             <Col span={24}>
-              {
-                task_status === 'checking' && (roles === 'admin' || creatBy?.id === user?.id  ) ?
-                    <>
-                      <Button type="primary" onClick={() => handleStatusDone(id)}>
-                        <IoMdCheckboxOutline />
-                      </Button>
-                      <Button color="danger" variant="solid" onClick={() => handleStatusProgress(id)}>
-                        <CgDanger />
-                      </Button>
-                    </>
-                    :
-                    <Tag color={'red'}>
-                      Checking
-                    </Tag>
+              {/*{*/}
+              {/*  roles === 'general' && task_status === "done" &&*/}
+              {/*    <Tag color="green">Done</Tag>*/}
+              {/*}*/}
+              {/*{*/}
+              {/*    roles === 'general' && task_status === "progress" &&*/}
+              {/*    <Tag color="green">progress</Tag>*/}
+              {/*}*/}
 
-              }
+
+              {/*{task_status === 'checking' && (roles === 'admin' || creatBy?.id === user?.id) ? (*/}
+              {/*    <>*/}
+              {/*      <Button type="primary" onClick={() => updateStatus(id, 'done')}>*/}
+              {/*        <IoMdCheckboxOutline /> Done*/}
+              {/*      </Button>*/}
+              {/*      <Button type="danger" onClick={() => updateStatus(id, 'progress')}>*/}
+              {/*        <CgDanger /> Set to Progress*/}
+              {/*      </Button>*/}
+              {/*    </>*/}
+              {/*) : task_status === 'checking' && task_status !== 'done' ? (*/}
+              {/*    <Tag color="red">Checking</Tag>*/}
+              {/*) : task_status !== 'done' ? (*/}
+              {/*    <Button type="primary" onClick={() => updateStatus(id, 'checking')}>*/}
+              {/*      <IoMdCheckboxOutline /> Set to Checking*/}
+              {/*    </Button>*/}
+              {/*) : (*/}
+              {/*    <Tag color="green">Done</Tag>*/}
+              {/*)}*/}
 
               {
-                task_status === 'checking' && task_status !== 'done' ?
-                    <Tag color={'red'}>
-                      Checking
-                    </Tag>
-                    :
-                    <Button type="primary" onClick={() => handleStatusChecking(id)}>
-                      <IoMdCheckboxOutline />
-                    </Button>
-              }
-              {
-                  task_status === 'done' &&
-                  <Tag color={'red'}>
-                    Done
-                  </Tag>
-
+                taskStatusChecking()
               }
             </Col>
+
             <Col span={20}>
-              <Flex gap={20} align={"start"}>
-                {/*<Popconfirm*/}
-                {/*    cancelText={'Отменить'}*/}
-                {/*    okText={'Завершить'}*/}
-                {/*    title={'Выполнить задачу?'}*/}
-                {/*    description={'Вы уверены, что хотите выполнить задачу?'}*/}
-                {/*    onConfirm={() => onChangeDoneProject(id)}*/}
-                {/*>*/}
-                {/*  <Checkbox checked={!!checkedState[id]} style={{marginRight: 8}}/>*/}
-                {/*</Popconfirm>*/}
-                <Flex align={"start"} gap={5} vertical={true}>
+              <Flex gap={20} align="start">
+                <Flex align="start" gap={5} vertical>
                   <Title level={4}>{title}</Title>
                   <Text>{text}</Text>
                 </Flex>
               </Flex>
             </Col>
+
             <Col span={4}>
-              <Flex align={"start"} justify={"end"} style={{width: '100%'}}>
+              <Flex align="start" justify="end" style={{ width: "100%" }}>
                 <Button type="primary" onClick={() => clickHandle(id)}>
-                  <FaRegCommentDots/>
+                  <FaRegCommentDots />
                 </Button>
-
-                {/*{*/}
-                {/*  roles === 'admin' ?*/}
-                {/*      task_status ==='checking' ?*/}
-                {/*      <Button type="primary" onClick={() => clickHandle(id)}>*/}
-                {/*        <IoMdCheckboxOutline />*/}
-                {/*      </Button>*/}
-                {/*          : ''*/}
-                {/*      :*/}
-                {/*      <Button type="primary" onClick={() => handleStatusProgress(id)}>*/}
-                {/*        <IoMdCheckboxOutline />*/}
-                {/*      </Button>*/}
-                {/*}*/}
-
-
               </Flex>
             </Col>
           </Row>
-
-
         </Spin>
       </div>
   );
 };
-export default TaskHeader
+
+export default TaskHeader;
